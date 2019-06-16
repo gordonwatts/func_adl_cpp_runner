e138d8c4ca3894274293961c0a905228bb97aa5c
Gordon Watts <gwatts@uw.edu>
2019-06-16 16:36:46 -0700

    Minor updates on starting
    - the ingester is now included
    - Updated how to start the ast injector with the proper queue name

--- scrpts/start_chain_in_docker.ps1
+++ scrpts/start_chain_in_docker.ps1
@@ -2,19 +2,31 @@
 # This is for debugging and testing. Kubernetes is probably the right place for this.
 # And at some point there will be a helm chart that will properly run all this.
 
+# These have to be determeind by hand when you start up your docker images for mongo and rabbit.
+$rabbitNode = "172.17.0.2"
+$mongoNode = "172.17.0.3"
+
 # How to insert an ast into the queue by hand.
 #  == "0" is the ast number (only 0 is supported so far)
 #  == "172.17.0.2" is the ip address of your docker container that is running rabbit. Use
 #     'docker inspect' as-rabbit to see what it is.
-# docker run --rm -it gordonwatts/func_adl:v0.0.1 python send_ast.py 0 172.17.0.2 find_did
+# docker run --rm -it gordonwatts/func_adl:v0.0.1 python send_ast.py 0 172.17.0.2 as_requests
 
 # Start up rabbit for all the interprocess communication.
 # Using default username/password (guest/guest) by going to http://localhost:15672.
 docker run --rm -d --hostname as-rabbit --name as-rabbit -p 15672:15672 rabbitmq:3-management
 
-# First go at getting the dataset downloader up and running.
+# Start up the mongodb.
+# Use `docker volume rm mondodata` to reset things.
+docker run --rm -d -v mongodata:/data/db -p:27000:27017  mongo:latest
+
+# Run the ingester, that takes simple requests and returns their status.
+# as_request -> find_did (if the info isn't in the database already)
+docker run -it --rm gordonwatts/func_adl_request_broker:v0.0.1 python request_ingester_rabbit.py $rabbitNode $mongoNode
+
+# Next is teh dataset downloader
 # rabbit find_did -> parse_cpp
-docker run --rm -it -v H:\OneDrive\.ssh\rucio-config\usercert:/root/rawcert -v G:\GRIDDocker:/data --name=desktop-rucio --rm -it --entrypoint /bin/bash  gordonwatts/desktop-rucio:alpha-0.1.3 startup_rabbit.sh gwatts mass2000 atlas file:///data 172.17.0.2
+docker run --rm -it -v H:\OneDrive\.ssh\rucio-config\usercert:/root/rawcert -v G:\GRIDDocker:/data --name=desktop-rucio --rm -it --entrypoint /bin/bash  gordonwatts/desktop-rucio:alpha-0.1.3 startup_rabbit.sh gwatts XXXX atlas file:///data $rabbitNode
 
 # Next, build the C++ code that is going to do our work
 # rabbit parse_cpp -> run_cpp
