8dbe1582f71e66a7465acdeb35663ed109e3454c
Gordon Watts <gwatts@uw.edu>
2019-06-16 23:27:33 -0700

    Update to use the new form of messages
    Got that all running. Sweet!

--- tools/cmd_runner_rabbit.py
+++ tools/cmd_runner_rabbit.py
@@ -9,7 +9,7 @@ import shutil
 # 1) This expects /cache to already be defined.
 # 2) This needs to run inside an ATLAS container. Which means python 2.
 
-def process_message(ch, method, properties, body):
+def process_message(xrootd_node, ch, method, properties, body):
     'Process each message and run the C++ for it.'
 
     # Make sure nothing from the previous job is sitting there waiting.
@@ -20,34 +20,50 @@ def process_message(ch, method, properties, body):
     r = json.loads(body)
 
     hash = r['hash']
+    code_hash = r['hash_source']
     main_script = r['main_script']
     input_files = r['files']
+    output_file = r['output_file']
+    xrootd_file = "root://" + xrootd_node + "//" + output_file
+    print xrootd_file
 
     with open('filelist.txt', 'w') as f:
         for f_name in input_files:
             f.write(f_name + '\n')
     
-    os.system(os.path.join('/cache', hash, main_script))
+    ch.basic_publish(exchange='', routing_key='status_change_state', body=json.dumps({'hash':hash, 'phase':'running'}))
+    os.system(os.path.join('/cache', code_hash, main_script) + " " + xrootd_file)
+    ch.basic_publish(exchange='', routing_key='status_change_state', body=json.dumps({'hash':hash, 'phase':'done'}))
+
+    # And send that done file on too.
+    ch.basic_publish(exchange='', routing_key='status_add_file', body=json.dumps({'hash':hash, 'file':output_file}))
 
     # This is as far as we go.
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-def listen_to_queue(rabbit_node):
+def listen_to_queue(rabbit_node, xrootd_node):
     'Get the various things downloaded and running'
 
     # Connect and setup the queues we will listen to and push once we've done.
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_node))
     channel = connection.channel()
-    channel.queue_declare(queue='run_cpp')
 
-    channel.basic_consume(queue='run_cpp', on_message_callback=process_message, auto_ack=False)
+    # We pull tasks off this guy.
+    channel.queue_declare(queue='run_cpp')
+    # Let them know about progress
+    channel.queue_declare(queue='status_change_state')
+    # Add files as they complete
+    channel.queue_declare(queue='status_add_file')
+
+    # Listen for work to show up.
+    channel.basic_consume(queue='run_cpp', on_message_callback=lambda ch, method, properties, body:process_message(xrootd_node, ch, method, properties, body), auto_ack=False)
 
     # We are setup. Off we go. We'll never come back.
     channel.start_consuming()
 
 if __name__ == '__main__':
-    bad_args = len(sys.argv) != 2
+    bad_args = len(sys.argv) != 3
     if bad_args:
-        print "Usage: python cmd_runner_rabbit.py <rabbit-mq-node-address>"
+        print "Usage: python cmd_runner_rabbit.py <rabbit-mq-node-address> <xrootd-results_node>"
     else:
-        listen_to_queue (sys.argv[1])
+        listen_to_queue (sys.argv[1], sys.argv[2])
